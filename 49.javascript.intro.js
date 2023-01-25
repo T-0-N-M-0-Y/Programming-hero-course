b3653e41a113143ca9b2e2869c56879bd5da7bfb
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-01-25 22:21:13 +0600

    New Work

---
+++ 49.javascript.intro.js
@@ -0,0 +1,20 @@
+/***
+ * Maths to USe
+ * 1. Number: 55, 6666, -34, -777
+ * 2. integer: 2, 6
+ * 3. float/decimal: 22.33, 44.77, 4.39
+ * 4. Operation: Addition, Substraction, Multiplication, Division, Remainder
+ * 5. Percent
+ * 6. Average
+ * 7. Comparison: <, >, ==, !=, >=, <=
+ * 8. Odd, Even
+ * 9. 45, 46, 70, 78, 122: max, min, avg, sum
+ * 10. Series
+ * 
+ * ::: EXTRA:::
+ * 1.Prime Number
+ * 2. Permutation
+ * 
+ *  ****/
+
+console.log(5555);
---
+++ 50.variable.js
@@ -0,0 +1,9 @@
+//This is How to Declare variable:
+
+//vary + able = variable
+
+var price = 11;
+var age = 72;
+var temperature = 36;
+var weight = 45;
+
