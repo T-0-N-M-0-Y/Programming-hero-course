8ecf718ed799ac6d33f99f66452741a9b92a6085
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-01-26 08:53:51 +0600

    New Work

--- 50.variable.js
+++ 50.variable.js
@@ -2,8 +2,43 @@
 
 //vary + able = variable
 
+//:::::variable types::::
+
+//Numeric
 var price = 11;
 var age = 72;
 var temperature = 36;
 var weight = 45;
 
+//String
+var person ="Tonmoy Asik";
+var location = 'Baacnakj akjcnac'
+
+//Boolean
+var ok = true;
+var bad = false;
+
+//variable names can't be any keyword
+var return = 00;
+var false = 90;
+
+//variable has to be in one word, no space between
+var price tag = 100;
+
+//Variable names can't have quotation
+var "name" = Tonmoy;
+
+//Variable name can't starts with a number but can ends with a number
+var 99clubs = okokoko;
+var clubs99 = okokokok;
+
+//How to use Long Names
+// can't use Dash
+var user-name = Tonmoy;
+
+//User under-score
+var user_name = Tonmoy;
+var user_home_address = bandor bon;
+
+//Another way to use long name
+var userHomeAddress = bandor bon; //camel case
