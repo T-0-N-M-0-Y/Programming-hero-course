c69810f129953ae5be4c814a984a25a6f910f5ae
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-01-27 11:54:31 +0600

    New Work

--- 58.conditional.js
+++ 58.conditional.js
@@ -37,3 +37,90 @@ if(chickenPrice < myMoney) {
 else{
     console.log('I will eat potato');
 }
+
+
+//Multiple Conditionals
+
+// var isGraduated = true;
+// var salary = 75000;
+// var cars = 1;
+
+// if(isGraduated == true){
+//     console.log('Eso biye kore feli')
+// }
+// else{
+//     console.log('tor Kopale Biya nai')
+// }
+
+// if(isGraduated == true && salary > 50000){
+//     console.log('Eso biye kore feli')
+// }
+// else{
+//     console.log('tor Kopale Biya nai')
+// }
+
+// if(isGraduated == true && salary > 50000 && cars >= 1){
+//     console.log('Eso biye kore feli')
+// }
+// else{
+//     console.log('tor Kopale Biya nai')
+// }
+
+
+var isGraduated = false;
+var salary = 61000;
+var cars = 0;
+
+if(isGraduated === true || salary > 50000 || cars >= 1){
+    console.log('Eso Prem kori');
+}
+else{
+    console.log('tomar friend er mobile number dao')
+}
+
+if ( isGraduated === true && (salary > 50000 || cars >=1)){
+
+}
+
+//Multi-stage Conditional
+
+var money = 68;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if(danishPrice < money) {
+    console.log('Dan Dan danish khamu');
+}
+else if (butterBread < money) {
+    console.log('Butter bon khamu')
+}
+else if(toastBiscuit < money){
+    console.log('chubai chubai toast biscuit khamu')
+}
+else {
+    console.log('Khali cha e sasther jonno valo')
+}
+
+//Nested Conditionals
+
+var math = true;
+var geometry = true;
+var straightLint = false;
+
+if(math == true){
+    if(geometry == true){
+        if(straightLint == true){
+
+        }
+        else{
+            console.log('baka pothe cholba na')
+        }
+    }
+    else{
+        console.log('pithagorous hoite parba na')
+    }
+}
+else{
+
+}
