ec46aae9426a71affc4e74de767200ca131d3324
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-01-27 22:36:30 +0600

    New Work

---
+++ 60.numbers.js
@@ -0,0 +1,23 @@
+//Display 1 to 10:
+
+var number = 1;
+while (number <= 10){
+    console.log(number);
+    number++
+}
+
+//Even Numbers
+
+var number = 0;
+while (number <= 10) {
+    console.log(number);
+    number = number +2;
+}
+
+//Odd Numbers
+
+var number = 1;
+while (number <= 10) {
+    console.log(number);
+    number = number +2;
+}
---
+++ 61.for.js
@@ -0,0 +1,40 @@
+var roastGiven = 0;
+while (roastGiven < 7) {
+    console.log('Roast Den, Please !!');
+    console.log(roastGiven);
+    roastGiven++;
+}
+
+// For Loop 
+
+for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+    console.log("Roast Den Vai!!!");
+    console.log(roastGiven);
+}
+
+//Simple Version Of For Loop
+
+for (var i = 0; i < 7; i++) {
+    console.log("Roast Den Vai!!!");
+    console.log(i); 
+}
+
+//Numbers 
+
+for(var i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+
+// Even Numbers 
+
+// i = i + 2;
+// i += 2;
+for(var i = 0; i <= 20; i+=2){
+    console.log(i);
+}
+
+//Odd Numbers
+for (var i = 1; i<=20; i+=2){
+    console.log(i);
+}
