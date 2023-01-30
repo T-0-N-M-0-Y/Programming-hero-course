c26aa4122a613d1e7383cd96adb0f8619d90e65d
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-01-30 17:38:35 +0600

    New Work

--- 69.object.js
+++ 69.object.js
@@ -43,14 +43,14 @@ var penCount2 = shoppingCart['pen'];
 
 var properties = Object.keys(shoppingCart);
 var propertyValues = Object.values(shoppingCart);
-// console.log(properties);
-// console.log(propertyValues);
+console.log(properties);
+console.log(propertyValues);
 console.log(shoppingCart);
 
 var propertyName = 'mouse';
 
 var propertyValue = shoppingCart[propertyName]
-// console.log(propertyName, propertyValue);
+console.log(propertyName, propertyValue);
 
 // set property values
 shoppingCart.mouse = 15;
@@ -59,3 +59,47 @@ shoppingCart['mouse'] = 29;
 console.log(shoppingCart);
 shoppingCart[propertyName] = 89;
 console.log(shoppingCart)
+
+// array vs object
+
+var shoppingItems = ['books', 'sunglass', 'shoes', 'pen'];
+var friendsAge = [12, 45, 78, 12, 32, 14];
+var friendsAge = {
+    rahim: 12,
+    samad: 45,
+    karim: 78,
+    jamal: 12,
+    robi:32,
+    kazi: 14
+}
+
+//Array  Loop
+
+var shoppingCart = {
+    books: 3, 
+    sunglass: 1,
+    keyboard: 5,
+    mouse: 1,
+    pen: 25,
+    shoes: 2
+}
+
+const keys = Object.keys(shoppingCart);
+console.log(keys);
+
+const values = Object.values(shoppingCart);
+console.log(values);
+
+// var keys = [ 'books', 'sunglass', 'keyboard', 'mouse', 'pen', 'shoes' ];
+for(var i = 0; i < keys.length; i++){
+    var propertyName = keys[i];
+    var propertyValue = shoppingCart[propertyName];
+    // console.log(propertyName, propertyValue);
+}
+
+// for in loop
+for(var propertyName in shoppingCart){
+    const value = shoppingCart[propertyName];
+    console.log(propertyName, value);
+
+}
