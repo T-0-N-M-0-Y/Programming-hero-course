32e1aa61058745bbbe6922fe4de338fe40d8bb24
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-02-09 10:33:30 +0600

    New Work

--- 98.matching.js
+++ 98.matching.js
@@ -5,30 +5,30 @@ const numbers = [45, 65, 23, 98, 19];
 //     console.log(number);
 // }
 
-for(const number of numbers){
+for (const number of numbers) {
     // console.log(number)
 }
 
 const products = [
-    {id: 1, name: 'xiami phone One night', price: 19000},
-    {id: 2, name: 'iphone', price: 19000},
-    {id: 3, name: 'mac book air', price: 119000},
-    {id: 4, name: 'lenovo yoga laptop 2025', price: 19000},
-    {id: 5, name: 'Dell inspiron laptop', price: 19000},
-    {id: 6, name: 'Samsung Phone note 7', price: 19000},
-    {id: 7, name: 'Nokia old age Phone gone', price: 19000},
-    {id: 8, name: 'Phone one', price: 19000},
-    {id: 8, name: 'M1 chip not cheap LapTOP', price: 19000},
+    { id: 1, name: 'xiami phone One night', price: 19000 },
+    { id: 2, name: 'iphone', price: 19000 },
+    { id: 3, name: 'mac book air', price: 119000 },
+    { id: 4, name: 'lenovo yoga laptop 2025', price: 19000 },
+    { id: 5, name: 'Dell inspiron laptop', price: 19000 },
+    { id: 6, name: 'Samsung Phone note 7', price: 19000 },
+    { id: 7, name: 'Nokia old age Phone gone', price: 19000 },
+    { id: 8, name: 'Phone one', price: 19000 },
+    { id: 8, name: 'M1 chip not cheap LapTOP', price: 19000 },
 ];
 
 // for(const product of products){
 //     console.log(product);
 // }
 
-function matchedProducts (products, search){
+function matchedProducts(products, search) {
     const matched = [];
-    for(const product of products){
-        if(product.name.toLowerCase().includes(search.toLowerCase())){
+    for (const product of products) {
+        if (product.name.toLowerCase().includes(search.toLowerCase())) {
             matched.push(product);
         }
     }
@@ -37,3 +37,44 @@ function matchedProducts (products, search){
 
 const result = matchedProducts(products, 'laptop');
 console.log(result);
+
+// quiz
+
+function compare(a, b) {
+
+    if (a == b) {
+
+        return true;
+
+    }
+
+    else {
+
+        return false;
+
+    }
+
+}
+
+const result2 = compare(15, "15");
+console.log(result2);
+
+//quiz 2
+
+function compare(a, b) {
+
+    if (a.toString() === b) {
+
+        return true;
+
+    } else {
+
+        return false;
+
+    }
+
+}
+
+const result3 = compare(25, '25');
+
+console.log(result3);
