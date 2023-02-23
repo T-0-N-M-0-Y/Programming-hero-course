ac9dd80b3bf14db35fe61ed91247e5989e7575f9
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-02-23 17:45:54 +0600

    update

---
+++ 126.fetch.js
@@ -0,0 +1,35 @@
+/* 
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json)) 
+
+  */
+
+
+// fetch(url)
+// .then(response => response.json())
+// .then(json => console.log(json))
+
+function loadData() {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    fetch(url)
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
+
+
+function loadData2(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUsers(data))
+}
+
+function displayUsers(data){
+    console.log(data);
+}
